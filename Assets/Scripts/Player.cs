17837b805a4b834d01e5844b53d39855c7fe022b
KorolER <elizavetakorol2005@gmail.com>
2024-11-10 22:06:59 +0300

    Input refactoring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] private float playerSpeed = 1f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = 1;
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * Time.deltaTime * playerSpeed;
         isWalking = moveDirection != Vector3.zero;
