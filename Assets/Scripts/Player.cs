3453836dae1d2ce033a3a24f3c52884466f40c72
KorolER <elizavetakorol2005@gmail.com>
2024-11-10 20:54:38 +0300

    Add Walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] private float playerSpeed = 1f;
+    private bool isWalking;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -23,9 +24,11 @@ public class Player : MonoBehaviour {
         inputVector = inputVector.normalized;
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-
         transform.position += moveDirection * Time.deltaTime * playerSpeed;
+        isWalking = moveDirection != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
+
+    public bool IsWalking() { return isWalking; }
 }
